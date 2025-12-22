8b14fe3c1b3721a956d5a6ce5cb7eb2a40cbaf16
code1218 <skjil1218@gmail.com>
2025-12-22 12:51:30 +0900

    get post sql 작성중

--- src/main/java/com/korit/post_mini_project_back/controller/PostController.java
+++ src/main/java/com/korit/post_mini_project_back/controller/PostController.java
@@ -1,14 +1,12 @@
 package com.korit.post_mini_project_back.controller;
 
 import com.korit.post_mini_project_back.dto.request.CreatePostReqDto;
+import com.korit.post_mini_project_back.dto.request.GetFeedListReqDto;
 import com.korit.post_mini_project_back.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -20,6 +18,11 @@ public class PostController {
     @PostMapping(value = "", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> createPost(@ModelAttribute CreatePostReqDto dto) {
         postService.createPost(dto);
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping("/feeds")
+    public ResponseEntity<?> getFeedList(GetFeedListReqDto dto) {
         return ResponseEntity.ok(null);
     }
 }
