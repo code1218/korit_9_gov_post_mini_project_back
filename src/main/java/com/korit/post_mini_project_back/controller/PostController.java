cd218c22526bd09a69f5684eae21deef41041797
code1218 <skjil1218@gmail.com>
2025-12-22 09:15:39 +0900

    post 등록

--- src/main/java/com/korit/post_mini_project_back/controller/PostController.java
+++ src/main/java/com/korit/post_mini_project_back/controller/PostController.java
@@ -1,6 +1,8 @@
 package com.korit.post_mini_project_back.controller;
 
 import com.korit.post_mini_project_back.dto.request.CreatePostReqDto;
+import com.korit.post_mini_project_back.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,11 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/posts")
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostService postService;
 
     @PostMapping(value = "", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> createPost(@ModelAttribute CreatePostReqDto dto) {
-        System.out.println(dto);
+        postService.createPost(dto);
         return ResponseEntity.ok(null);
     }
 }
--- src/main/java/com/korit/post_mini_project_back/security/PrincipalUser.java
+++ src/main/java/com/korit/post_mini_project_back/security/PrincipalUser.java
@@ -4,8 +4,10 @@ import com.korit.post_mini_project_back.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -22,4 +24,20 @@ public class PrincipalUser extends DefaultOAuth2User {
         super(authorities, attributes, nameAttributeKey);
         this.user = user;
     }
+
+    public static PrincipalUser getAuthenticatedPrincipalUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PrincipalUser principalUser = (PrincipalUser) authentication.getPrincipal();
+        return principalUser;
+    }
 }
+
+
+
+
+
+
+
+
+
+
